db07a2677cbc1807cce679467ed2854ddc7a19fb
kelompok 1 <fyznugraha@gmail.com>
2026-07-16 17:05:34 +0700

    add gotunes image

--- src/i18n/locales/en.ts
+++ src/i18n/locales/en.ts
@@ -86,7 +86,7 @@ export const en = {
       { name: "BJB Band", image: "/artis/bjb.png", tier: "supporting", imagePosition: "center 35%" },
       { name: "D'Korp", image: "/artis/d korpp.png", tier: "supporting", imagePosition: "top" },
       { name: "Dinda Alvian/ Screaming Hedgehog", image: "/artis/SCREAMING HEDGEHOG.jpg", tier: "supporting", imagePosition: "center" },
-      { name: "Go Tunes", image: "/artis/gotunes.png", tier: "supporting", imagePosition: "center" },
+      { name: "Go Tunes", image: "/artis/gotunes.jpg", tier: "supporting", imagePosition: "center" },
       { name: "Pasmatik", image: "/artis/pasmatik.jpg", tier: "supporting", imagePosition: "center" },
       { name: "Rico Ceper", image: "/artis/riko.jpg", tier: "supporting", imagePosition: "center" },
       { name: "Samsaka", image: "/artis/samsaka.jpg", tier: "supporting", imagePosition: "center" },
